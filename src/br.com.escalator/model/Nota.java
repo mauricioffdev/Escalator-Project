316592aa68f5c1da0425d4de758dff867e1cc093
mauricioffdev <mafilhoguitar@gmail.com>
2025-10-09 11:05:11 -0300

    Update Nota.java

--- src/br.com.escalator/model/Nota.java
+++ src/br.com.escalator/model/Nota.java
@@ -2,7 +2,6 @@ package br.com.escalator.model;
 
 import java.util.Map;
 
-// src/main/java/br/com/escalator/model/Nota.java
 public enum Nota {
     C(0), C_SHARP(1), D(2), D_SHARP(3), E(4), F(5), F_SHARP(6),
     G(7), G_SHARP(8), A(9), A_SHARP(10), B(11);
